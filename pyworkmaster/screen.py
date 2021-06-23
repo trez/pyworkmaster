6e5ac254086c222d8a297aa9484797148521bb07
trez <tobias.vehkajarvi@gmail.com>
2021-06-23 14:00:46 +0000

    Small code cleanups

--- pyworkmaster/screen.py
+++ pyworkmaster/screen.py
@@ -28,11 +28,11 @@ def run(config, proj):
     # Create layout.
     queue = [p['layout']]
     while queue:
-        n = queue[0]
-        queue = queue[1:]
+        n, *queue = queue
         if not n.name.isalpha():
             num_splits = len(n.children)-1
-            screenrc.extend([f"split{' -v' if n.name == '|' else ''}"]*num_splits)
+            split = 'split -v' if n.name == '|' else 'split'
+            screenrc.extend([f"{split}"]*num_splits)
         else:
             screenrc.append(f"select {n.name}")
             screenrc.append("focus")
@@ -42,8 +42,7 @@ def run(config, proj):
     # Write to temp config.
     fd, tmpfile = tempfile.mkstemp()
     with open(tmpfile, "w") as f:
-        for line in screenrc:
-            f.write(f"{line}\n")
+        f.write("\n".join(screenrc))
 
     # Start screen session.
     subprocess.run(f"screen -c {tmpfile}", shell=True)
